b853a7d79d7ab7babe99bbd515588b66314e922f
Eric Kelly <heroiceric@gmail.com>
2014-02-03 10:52:18 -0500

    Add 'Odd Numbers' challenge

---
+++ exercises/ruby/flow_control/odd_numbers/odd_numbers_test.rb
@@ -0,0 +1,64 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+
+require_relative 'odd_numbers'
+
+describe "Odd Numbers" do
+  it "prints all odd numbers from 1 to 100" do
+    expected_output = <<-eos
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23
+25
+27
+29
+31
+33
+35
+37
+39
+41
+43
+45
+47
+49
+51
+53
+55
+57
+59
+61
+63
+65
+67
+69
+71
+73
+75
+77
+79
+81
+83
+85
+87
+89
+91
+93
+95
+97
+99
+    eos
+
+    code = File.read('odd_numbers.rb')
+    proc { eval(code) }.must_output expected_output
+  end
+end
