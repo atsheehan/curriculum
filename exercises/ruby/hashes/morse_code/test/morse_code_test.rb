df530bfc484981564ceecef1969c64d7f98c4185
Adam Sheehan <a.t.sheehan@gmail.com>
2014-02-15 12:50:08 -0500

    Reorg morse code kata.

--- exercises/ruby/hashes/morse_code/test/morse_code_test.rb
+++ exercises/ruby/hashes/morse_code/test/morse_code_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/spec'
 require 'minitest/autorun'
 
-require_relative './morse_code'
+require_relative '../lib/morse_code'
 
 describe 'morse code' do
   it 'decodes a secret morse code message' do
@@ -10,4 +10,3 @@ describe 'morse code' do
     decode(code).must_equal "HELP I'M TRAPPED IN THE COMPUTER! THE PASSWORD IS 32589"
   end
 end
-
