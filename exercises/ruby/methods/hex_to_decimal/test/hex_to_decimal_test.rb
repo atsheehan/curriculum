bb6ca5da0f195a478a6ea0d78b964ddb80620c22
Adam Sheehan <a.t.sheehan@gmail.com>
2014-02-15 13:17:16 -0500

    Reorg hex to decimal kata.

--- exercises/ruby/methods/hex_to_decimal/test/hex_to_decimal_test.rb
+++ exercises/ruby/methods/hex_to_decimal/test/hex_to_decimal_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/spec'
 require 'minitest/autorun'
 
-require_relative 'hex_to_decimal'
+require_relative '../lib/hex_to_decimal'
 
 describe "#hex_to_decimal" do
   it "converts single number from hex to decimal" do
@@ -9,27 +9,22 @@ describe "#hex_to_decimal" do
   end
 
   it "converts single letter from hex to decimal" do
-    skip
     hex_to_decimal('E').must_equal 14
   end
 
   it "converts multiple numbers" do
-    skip
     hex_to_decimal('5A').must_equal 90
   end
 
   it "converts multiple letters" do
-    skip
     hex_to_decimal('CC').must_equal 204
   end
 
   it "converts multiple numbers and letters" do
-    skip
     hex_to_decimal('1A2B3C').must_equal 1715004
   end
 
   it "is case insensitive" do
-    skip
     hex_to_decimal('1a2b3c').must_equal 1715004
   end
 end
