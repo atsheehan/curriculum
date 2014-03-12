e3c011f06751fbfc6177e0e613206b774e0e2b8a
Eric Kelly <heroiceric@gmail.com>
2014-03-12 11:58:27 -0400

    Convert "Multiply Lists" to rspec

--- exercises/ruby/iteration_and_arrays/multiply_lists/test/multiply_lists_test.rb
+++ exercises/ruby/iteration_and_arrays/multiply_lists/test/multiply_lists_test.rb
@@ -1,6 +1,3 @@
-require 'minitest/spec'
-require 'minitest/autorun'
-
 require_relative '../lib/multiply_lists'
 
 describe 'multiply lists' do
@@ -8,22 +5,23 @@ describe 'multiply lists' do
     input = "7 0 2 | 19 44 12"
 
     output = multiply_lists(input)
-    output.must_equal "133 0 24"
+
+    expect(output).to eq "133 0 24"
   end
 
   it 'multiplies lists of single integers' do
-    skip
     input = "70 | 7"
 
     output = multiply_lists(input)
-    output.must_equal "490"
+
+    expect(output).to eq "490"
   end
 
   it 'returns error message if lists are not same length' do
-    skip
     input = "29 8 3 | 7 2"
 
     output = multiply_lists(input)
-    output.must_equal "Lists must be same length"
+
+    expect(output).to eq "Lists must be same length"
   end
 end
