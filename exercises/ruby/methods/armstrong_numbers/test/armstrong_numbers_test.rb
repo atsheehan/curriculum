ccd8c59b6d577ba4a91370b935e18f3300b56c2f
Eric Kelly <heroiceric@gmail.com>
2014-03-12 13:03:31 -0400

    Convert "Armstrong Numbers" to rspec

--- exercises/ruby/methods/armstrong_numbers/test/armstrong_numbers_test.rb
+++ exercises/ruby/methods/armstrong_numbers/test/armstrong_numbers_test.rb
@@ -1,47 +1,40 @@
-require 'minitest/spec'
-require 'minitest/autorun'
-
 require_relative '../lib/armstrong_numbers'
 
 describe "#armstrong_number?" do
   it "correctly identifies armstrong numbers of length 1" do
     0.upto(9) do |n|
-      armstrong_number?(n).must_equal true
+      expect(armstrong_number?(n)).to eq true
     end
   end
 
   it "correctly identifies non-armstrong numbers of length 2" do
-    skip
     10.upto(99) do |n|
-      armstrong_number?(n).must_equal false
+      expect(armstrong_number?(n)).to eq false
     end
   end
 
   it "correctly identifies armstrong numbers of length 3" do
-    skip
-    armstrong_number?(153).must_equal true
-    armstrong_number?(370).must_equal true
-    armstrong_number?(371).must_equal true
+    expect(armstrong_number?(153)).to eq true
+    expect(armstrong_number?(370)).to eq true
+    expect(armstrong_number?(371)).to eq true
   end
 
   it "correctly identifies non-armstrong numbers of length 3" do
-    skip
-    armstrong_number?(302).must_equal false
-    armstrong_number?(404).must_equal false
-    armstrong_number?(500).must_equal false
+    expect(armstrong_number?(302)).to eq false
+    expect(armstrong_number?(404)).to eq false
+    expect(armstrong_number?(418)).to eq false
+    expect(armstrong_number?(500)).to eq false
   end
 
   it "correctly identifies armstrong numbers of length 5" do
-    skip
-    armstrong_number?(54748).must_equal true
-    armstrong_number?(92727).must_equal true
-    armstrong_number?(93084).must_equal true
+    expect(armstrong_number?(54748)).to eq true
+    expect(armstrong_number?(92727)).to eq true
+    expect(armstrong_number?(93084)).to eq true
   end
 
   it "correctly identifies non-armstrong numbers of length 5" do
-    skip
-    armstrong_number?(30278).must_equal false
-    armstrong_number?(40465).must_equal false
-    armstrong_number?(50035).must_equal false
+    expect(armstrong_number?(30278)).to eq false
+    expect(armstrong_number?(40465)).to eq false
+    expect(armstrong_number?(50035)).to eq false
   end
 end
