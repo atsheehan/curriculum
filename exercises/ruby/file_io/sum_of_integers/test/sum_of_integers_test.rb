242eb8f68db891da32714302f45ffa8fe27aadc7
Eric Kelly <heroiceric@gmail.com>
2014-03-12 11:24:55 -0400

    Convert "Sum of Integers" to rspec

--- exercises/ruby/file_io/sum_of_integers/test/sum_of_integers_test.rb
+++ exercises/ruby/file_io/sum_of_integers/test/sum_of_integers_test.rb
@@ -1,11 +1,8 @@
-require 'minitest/spec'
-require 'minitest/autorun'
-
 require_relative '../lib/sum_of_integers'
 
 describe "#sum" do
   it "sums up a file of integers" do
     sample_file = File.join(File.dirname(__FILE__), 'integers.txt')
-    sum(sample_file).must_equal 55
+    expect(sum(sample_file)).to eq 55
   end
 end
