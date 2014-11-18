9b05e61755b828ebdbbdc679212a4d6944f627b4
Richard Davis <rdavis.bacs@gmail.com>
2014-11-18 10:56:43 -0500

    pagination solution

--- lessons/pagination/pagination.rb
+++ lessons/pagination/pagination.rb
@@ -1,3 +1,28 @@
-def paginate(current_page, results_count, max_pages, results_per_page)
-  # YOUR CODE HERE
+def paginate(current_page, results_count, max_pages = 10, results_per_page = 10)
+  result = []
+  result << current_page
+
+  total_pages = (results_count / results_per_page.to_f).ceil
+
+  i = 1
+  while result.length < total_pages && result.length < max_pages
+    page = 0
+    if i % 2 == 0
+      page = result.first - 1
+    else
+      page = result.last + 1
+    end
+
+    if page > 0 && page <= total_pages
+      if page < current_page
+        result.unshift(page)
+      else
+        result.push(page)
+      end
+    end
+
+    i += 1
+  end
+
+  return result
 end
