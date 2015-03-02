89d6d6c3dad84a0965d52d6a03bc82ff71acb183
Vikram Ramakrishnan <lord.ezar@gmail.com>
2015-03-02 15:44:25 -0500

    add evening exercise for database relationships & foreign keys

---
+++ lessons/evening-brussels-sprouts-comments/seeder.rb
@@ -0,0 +1,15 @@
+require 'pg'
+
+TITLES = ["Roasted Brussels Sprouts",
+  "Fresh Brussels Sprouts Soup",
+  "Brussels Sprouts with Toasted Breadcrumbs, Parmesan, and Lemon",
+  "Cheesy Maple Roasted Brussels Sprouts and Broccoli with Dried Cherries",
+  "Hot Cheesy Roasted Brussels Sprout Dip",
+  "Pomegranate Roasted Brussels Sprouts with Red Grapes and Farro",
+  "Roasted Brussels Sprout and Red Potato Salad",
+  "Smoky Buttered Brussels Sprouts",
+  "Sweet and Spicy Roasted Brussels Sprouts",
+  "Smoky Buttered Brussels Sprouts",
+  "Brussels Sprouts and Egg Salad with Hazelnuts"]
+
+#WRITE CODE TO SEED YOUR DATABASE AND TABLES HERE
