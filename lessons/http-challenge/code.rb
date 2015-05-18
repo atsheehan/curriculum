c37f31c39eaba88f23ec60fcc144eaf69ba96bb7
Richard Davis <rdavis.bacs@gmail.com>
2015-05-18 15:43:43 -0400

    update assignment instructions

--- lessons/http-challenge/code.rb
+++ lessons/http-challenge/code.rb
@@ -12,7 +12,11 @@ HTTP Challenge
 * Use the tool of your choice to draw the HTTP request/response cycle.
   - include a link to your diagram, here
 
-* Use telnet, curl, or ruby code to create a new message at launch-academy-chat.herokuapp.com
+* Use telnet, curl, or ruby code to retrieve the messages at
+  launch-academy-chat.herokuapp.com/messages
+
+* Use telnet, curl, or ruby code to create a new message at
+  launch-academy-chat.herokuapp.com/messages
 
   - Kernel::system method in ruby will allow you to execute command-line utilities such as curl.
   - other useful ruby libraries:
