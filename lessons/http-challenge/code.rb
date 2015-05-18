7f59c383dc0424d0961c77b4f4c5ea23d2f31dd2
Richard Davis <rdavis.bacs@gmail.com>
2015-05-18 15:42:30 -0400

    add submission file for HTTP Challenge

---
+++ lessons/http-challenge/code.rb
@@ -0,0 +1,24 @@
+<<-DOC
+
+HTTP Challenge
+--------------
+
+* Define the common HTTP verbs
+  - GET:
+  - POST:
+  - PUT/PATCH:
+  - DELETE:
+
+* Use the tool of your choice to draw the HTTP request/response cycle.
+  - include a link to your diagram, here
+
+* Use telnet, curl, or ruby code to create a new message at launch-academy-chat.herokuapp.com
+
+  - Kernel::system method in ruby will allow you to execute command-line utilities such as curl.
+  - other useful ruby libraries:
+    * Net::HTTP
+    * Net::Telnet
+
+DOC
+
+# code goes here
